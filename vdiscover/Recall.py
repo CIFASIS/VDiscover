b6a10590198e363ac39681e410f21029e502a691
gaa-cifasis <gg@cifasis-conicet.gov.ar>
2015-06-19 09:23:52 +0000

    Optimized recall and testing speed

--- vdiscover/Recall.py
+++ vdiscover/Recall.py
@@ -9,7 +9,7 @@ csv.field_size_limit(sys.maxsize)
 
 def Recall(model_file, in_file, in_type, out_file, probability=False, test=False):
 
-  test_classes,predicted_classes = [],[]
+  testcases,test_classes = [],[]
 
   if ".gz" in in_file:
     infile = gzip.open(in_file, "r")
@@ -23,28 +23,33 @@ def Recall(model_file, in_file, in_type, out_file, probability=False, test=False
 
   model = pickle.load(gzip.open(model_file))
 
-  for row in csvreader:
-    prog, features = row[0], row[1]
+  x = dict()
+  x[in_type] = []
 
-    x = dict()
-    x[in_type] = [features]
-    if probability:
-      y = [model.predict_proba(x)[0][1]] # probability of the second class
-    else:
-      y = [model.predict(x)[0]]
-    csvwriter.writerow([prog]+y)
+  for row in csvreader:
+    testcase, features = row[0], row[1]
+    testcases.append(testcase)
 
     if test:
       test_classes.append(int(row[2]))
-      predicted_classes.append(y[0])
+
+    x[in_type].append(features)
+
+  if probability:
+    predicted_classes = map(lambda x: x[1], model.predict_proba(x)) # probability of the second class
+  else:
+    predicted_classes = model.predict(x)
+
+  for testcase,y in zip(testcases,predicted_classes):
+    csvwriter.writerow([testcase,y])
 
   if test:
     #print confusion_matrix(test_classes, predicted_classes)
     nclasses = len(set(test_classes))
-    
+
     if nclasses == 1:
       err = [None, None]
-      err[test_classes[0]] = recall_score(test_classes, predicted_classes)
+      err[test_classes[0]] = recall_score(test_classes, predicted_classes, average=None)[0]
       err[1 - test_classes[0]] = 1.0
     else: 
       err = recall_score(test_classes, predicted_classes, average=None)
