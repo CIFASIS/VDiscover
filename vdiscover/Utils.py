e88028b8f258cca07dbeead8798ce2627ea5e408
gaa-cifasis <gg@cifasis-conicet.gov.ar>
2015-10-19 11:18:53 +0000

    added progress bar code

--- vdiscover/Utils.py
+++ vdiscover/Utils.py
@@ -16,12 +16,33 @@ along with VDISCOVER. If not, see <http://www.gnu.org/licenses/>.
 
 Copyright 2014 by G.Grieco
 """
-
+import sys
+import time
 import gzip
 import subprocess
 import pickle
 import csv
 import random
+
+
+def update_progress(progress):
+    barLength = 30 # Modify this to change the length of the progress bar
+    status = ""
+    if isinstance(progress, int):
+        progress = float(progress)
+    if not isinstance(progress, float):
+        progress = 0
+        status = "error: progress var must be float\r\n"
+    if progress < 0:
+        progress = 0
+        status = "Halt...\r\n"
+    if progress >= 1:
+        progress = 1
+        status = "Done...\r\n"
+    block = int(round(barLength*progress))
+    text = "\rPercent: [{0}] {1}% {2}".format( "#"*block + "-"*(barLength-block), progress*100, status)
+    sys.stdout.write(text)
+    sys.stdout.flush()
 
 def file_len(fname):
 
